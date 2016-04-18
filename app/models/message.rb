a1b85d106e0ad8b9300a894ad8483756467871bd
yukamiyazawa <info@o2rm.com>
2016-04-18 08:35:57 +0000

    heroku-migration

--- app/models/message.rb
+++ app/models/message.rb
@@ -3,4 +3,7 @@ class Message < ActiveRecord::Base
     validates :name ,length: { maximum: 20 } , presence: true
     #内容は必須入力かつ2文字以上30文字以下
     validates :body , length: { minimum: 2 , maximum: 30 } , presence: true
+    #年齢は０以上の数字のみ入力
+    #validates :age , greater_than_or_equal_to: 0, numericality: true
+    validates :age , numericality: { only_integer: true,greater_than_or_equal_to: 0 }
 end
