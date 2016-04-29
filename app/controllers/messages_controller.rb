d12fd289a71aeab658b3de94e65a806e17329383
yukamiyazawa <info@o2rm.com>
2016-04-29 14:09:59 +0000

    challenge2

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -21,7 +21,12 @@ class MessagesController < ApplicationController
     end
   end
   
+  #def age
+  #  if @message.age
+  #  end
+  
   def create
+    #binding.pry
     @message = Message.new(message_params)
     if @message.save
       redirect_to root_path , notice: 'メッセージを保存しました'
@@ -42,7 +47,7 @@ class MessagesController < ApplicationController
   
   private
   def message_params
-    params.require(:message).permit(:name, :body)
+    params.require(:message).permit(:name, :age, :body)
   end
    ##ここまで
   def set_message
